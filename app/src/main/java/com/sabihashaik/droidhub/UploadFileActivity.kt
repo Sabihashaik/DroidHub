2dfa864c05a7e70191f18689f2f9eb048de31848
sabihashaik <sabihaabc1@gmail.com>
2020-12-20 22:18:34 +0400

    Add firebase storage

--- app/src/main/java/com/sabihashaik/droidhub/UploadFileActivity.kt
+++ app/src/main/java/com/sabihashaik/droidhub/UploadFileActivity.kt
@@ -1,11 +1,22 @@
 package com.sabihashaik.droidhub
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
 
 class UploadFileActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload_file)
+
+        var storage = Firebase.storage
+        var storageRef = storage.reference
+
+
+
     }
 }
