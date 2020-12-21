02baecddbd62050c42a145faf08eb4c05f7fe672
Sabiha S <sabihaabc1@gmail.com>
2020-12-21 13:13:58 +0400

    Add RecyclerView Components

---
+++ app/src/main/java/com/sabihashaik/droidhub/adapter/FileListAdapter.kt
@@ -0,0 +1,4 @@
+package com.sabihashaik.droidhub.adapter
+
+class FileListAdapter {
+}
---
+++ app/src/main/java/com/sabihashaik/droidhub/data/fileItem.kt
@@ -0,0 +1,8 @@
+package com.sabihashaik.droidhub.data
+
+import android.net.Uri
+
+data class fileItem(
+        var name:String,
+        var downloadURL: Uri
+)
