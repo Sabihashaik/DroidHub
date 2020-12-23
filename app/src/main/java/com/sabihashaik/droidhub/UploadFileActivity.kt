cf73a75088fe4e88d8bd0677bbabd5e78b558475
Sabiha S <sabihaabc1@gmail.com>
2020-12-23 14:24:55 +0400

    Update downloadURI for image file

--- app/src/main/java/com/sabihashaik/droidhub/UploadFileActivity.kt
+++ app/src/main/java/com/sabihashaik/droidhub/UploadFileActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -13,7 +14,6 @@ import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.UploadTask
 import com.google.firebase.storage.ktx.storage
 import com.sabihashaik.droidhub.databinding.ActivityUploadFileBinding
 
@@ -37,7 +37,7 @@ class UploadFileActivity : AppCompatActivity() {
         if(requestCode == PICK_IMAGE_CODE && resultCode== RESULT_OK && data!=null){
             filePath = data.data!!
             binding.FilePathText.text = "File Path: "+filePath
-            Log.d("DroidHub","File Path: "+filePath)
+            Log.d("DroidHub", "File Path: " + filePath)
         }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,27 +70,32 @@ class UploadFileActivity : AppCompatActivity() {
 
     private fun uploadFile() {
         val fileName = binding.fileNameField.text.toString()
-        val myFileRef = mStorageRef?.child("images/"+fileName)
+        val myFileRef = mStorageRef?.child("images/" + fileName)
         val uploadTask = filePath?.let { myFileRef?.putFile(it) }
 
         uploadTask?.addOnFailureListener {
-            Toast.makeText(this, "Task Failed"+it, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Task Failed" + it, Toast.LENGTH_SHORT).show()
          }?.addOnSuccessListener { taskSnapshot ->
-            val downloadUri = uploadTask.result
-            addItemtoFireStore(fileName,downloadUri)
-            Toast.makeText(this, "Task Succeeded"+downloadUri, Toast.LENGTH_SHORT).show()
+
+            val result: Task<Uri> = taskSnapshot.storage.downloadUrl
+            result.addOnSuccessListener { uri ->
+                val downloadUri = uri.toString()
+                addItemtoFireStore(fileName,downloadUri)
+            }
+
+            Toast.makeText(this, "Task Succeeded" , Toast.LENGTH_SHORT).show()
         }
 
     }
 
-    private fun addItemtoFireStore(fileName:String,downloadUri: UploadTask.TaskSnapshot) {
+    private fun addItemtoFireStore(fileName: String, downloadUri: String) {
         Log.d("DroidHub", "Entered Adding Item to Firestore")
         val userId = auth.uid.toString()
         //content://com.android.providers.media.documents/document/image%3A63996
         var downloadUri2 = downloadUri.toString()
         var docs = hashMapOf(
-            "downloadURL" to downloadUri2,
-            "filename" to fileName
+                "downloadURL" to downloadUri2,
+                "filename" to fileName
         )
         //val collectionPath = "users/"+userId+"/documents"
 
@@ -105,8 +110,8 @@ class UploadFileActivity : AppCompatActivity() {
                         Log.d("DroidHub", "Error adding document", e)
                     }
         }
-        catch(e:Exception){
-            Log.d("DroidHub","Oops!"+e)
+        catch (e: Exception){
+            Log.d("DroidHub", "Oops!" + e)
         }
     }
 
